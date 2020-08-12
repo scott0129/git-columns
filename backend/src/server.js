eb62e461b4331e9519eba31240318509b3168633
Scott Kim <scott102938@gmail.com>
2020-08-12 21:01:38 +0900

    Update gitignore for .env secrets

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,15 @@ const http = require('http');
 const path = require('path');
 const app = express();
 const server = http.Server(app);
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
 
 const portNumber = process.env.PORT || 5000;
+
+let clientKey = '';
+let clientSecret = '';
+
+if (clientKey
 
 app.use('/', express.static(path.join(__dirname, '../../frontend/dist/')));
 server.listen(portNumber, function () {
